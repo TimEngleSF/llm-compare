6807a25da4702e8104defc3faf4a867e15f3d8e5
Tim Engle <timothy.englew@gmail.com>
2024-05-15 10:28:09 -0700

    Create base class that utilize OpenAI client

--- client/OpenAI.py
+++ client/OpenAI.py
@@ -10,45 +10,26 @@ OPENAI_DEFAULT_MODEL = "gpt-4-turbo-preview"
 OPENAI_DEFAULT_MODEL_NAME = "GPT-4 Turbo"
 
 
-class OpenAIClient(Client):
-    """
-    A client class for interacting with the OpenAI API.
-    """
-
-    def __init__(self, api_key: str):
-        """
-        Initializes an instance of the OpenAIClient.
-
-        Args:
-            api_key (str): The API key for accessing the OpenAI API.
-        """
+class BaseOpenAIClient(Client):
+    def __init__(
+        self,
+        api_key: str,
+        client: AsyncOpenAI,
+        model: str,
+        model_name: str,
+    ):
         super().__init__(api_key)
-        self.client = AsyncOpenAI(api_key=api_key)
-        self.model_dict = {
-            "gpt-4-turbo-preview": "GPT-4 Turbo",
-            "gpt-4o": "GPT-4o",
-        }
+        self.client = client
+        self.model = model
+        self.model_name = model_name
         self.temp = 1.0
         self.temp_max = 2.0
         self.temp_min = 0.0
 
-    async def merge(
-        self,
-        obj,
-        model="gpt-4-turbo-preview",
-    ):
-        """
-        Merges the given object using the OpenAI API.
-
-        Args:
-            obj: The object to be merged.
-            model (str): The model to use for merging. Defaults to "gpt-4-turbo-preview".
-
-        Returns:
-            dict: A dictionary containing the merged output, model name, execution time, and temperature.
-        """
+    async def merge(self, obj, model=None):
+        if model is None:
+            model = self.model
         start_time = time.time()
-
         response = await self.client.chat.completions.create(
             model=model,
             messages=[
@@ -56,65 +37,58 @@ class OpenAIClient(Client):
                     "role": "system",
                     "content": merge_prompt,
                 },
-                {"role": "user", "content": json.dumps({"ingredients": obj})},
+                {
+                    "role": "user",
+                    "content": json.dumps({"ingredients": obj}),
+                },
             ],
-            response_format={"type": "json_object"},
             temperature=self.temp,
         )
         end_time = time.time()
         execution_time = round(end_time - start_time, 4)
-
-        model_name = (
-            OPENAI_DEFAULT_MODEL_NAME if model == OPENAI_DEFAULT_MODEL else model
-        )
-
+        model_name = self.model_name if model == self.model else model
         data = {
             "model": model_name,
             "output": json.loads(response.choices[0].message.content),
             "execution_time": execution_time,
             "temp": self.temp,
         }
-
         return data
 
-    async def split(self, obj, model="gpt-4-turbo-preview"):
-        """
-        Splits the given object using the OpenAI API.
-
-        Args:
-            obj: The object to be split.
-            model (str): The model to use for splitting. Defaults to "gpt-4-turbo-preview".
-
-        Returns:
-            dict: A dictionary containing the split output, model name, execution time, and temperature.
-        """
+    async def split(self, obj, model=None):
+        if model is None:
+            model = self.model
         start_time = time.time()
-
         response = await self.client.chat.completions.create(
-            model="gpt-4-turbo-preview",
+            model=model,
             messages=[
                 {
                     "role": "system",
                     "content": split_prompt,
                 },
-                {"role": "user", "content": f"Split this object: {obj}"},
+                {
+                    "role": "user",
+                    "content": f"Split this object: {obj}",
+                },
             ],
-            response_format={"type": "json_object"},
             temperature=self.temp,
         )
-
         end_time = time.time()
         execution_time = round(end_time - start_time, 4)
-
-        model_name = (
-            OPENAI_DEFAULT_MODEL_NAME if model == OPENAI_DEFAULT_MODEL else model
-        )
-
+        model_name = self.model_name if model == self.model else model
         data = {
             "model": model_name,
             "output": json.loads(response.choices[0].message.content),
             "execution_time": execution_time,
             "temp": self.temp,
         }
-
         return data
+
+
+# OpenAIClient class
+class OpenAIClient(BaseOpenAIClient):
+    def __init__(self, api_key: str):
+        client = AsyncOpenAI(api_key=api_key)
+        default_model = OPENAI_DEFAULT_MODEL
+        default_model_name = OPENAI_DEFAULT_MODEL_NAME
+        super().__init__(api_key, client, default_model, default_model_name)
--- client/OpenRouter.py
+++ client/OpenRouter.py
@@ -1,10 +1,5 @@
 from openai import AsyncOpenAI
-import json
-import time
-
-
-from client.Client import Client
-from client.sys_prompts import merge_prompt, split_prompt
+from client.OpenAI import BaseOpenAIClient
 
 LLAMA_MODELS = {
     "llama-3-70b": "meta-llama/llama-3-70b-instruct",
@@ -17,73 +12,36 @@ OPENROUTER_DEFAULT_MODEL_NAME = "Llama3 70b"
 OPENROUTER_API_URL = "https://openrouter.ai/api/v1"
 
 
-class OpenRouterClient(Client):
-    def __init__(self, api_key: str):
-        super().__init__(api_key)
-        self.client = AsyncOpenAI(
-            api_key=api_key,
-            base_url=OPENROUTER_API_URL,
-        )
-        self.temp = 1.0
-        self.temp_max = 2.0
-        self.temp_min = 0.0
+class OpenRouterClient(BaseOpenAIClient):
+    """
+    A client for interacting with the OpenRouter API.
 
-    async def merge(self, obj, model=OPENROUTER_DEFAULT_MODEL, name="Llama 3"):
-        start_time = time.time()
-        response = await self.client.chat.completions.create(
-            model=model,
-            messages=[
-                {
-                    "role": "system",
-                    "content": merge_prompt,
-                },
-                {
-                    "role": "user",
-                    "content": json.dumps({"ingredients": obj}),
-                },
-            ],
-            temperature=self.temp,
-        )
-        end_time = time.time()
-        execution_time = round(end_time - start_time, 4)
-        model_name = (
-            OPENROUTER_DEFAULT_MODEL_NAME if model == OPENROUTER_DEFAULT_MODEL else name
-        )
-        data = {
-            "model": model_name,
-            "output": json.loads(response.choices[0].message.content),
-            "execution_time": execution_time,
-            "temp": self.temp,
-        }
-        return data
+    Args:
+        api_key (str): The API key for authentication.
 
-    async def split(self, obj, model=OPENROUTER_DEFAULT_MODEL):
-        start_time = time.time()
-        response = await self.client.chat.completions.create(
-            model=model,
-            messages=[
-                {
-                    "role": "system",
-                    "content": split_prompt,
-                },
-                {
-                    "role": "user",
-                    "content": f"Split this object: {obj}",
-                },
-            ],
-            temperature=self.temp,
+    Attributes:
+        client (AsyncOpenAI): An instance of the AsyncOpenAI client.
+        default_model (str): The default model to use for requests.
+        default_model_name (str): The name of the default model.
+
+    """
+
+    def __init__(
+        self,
+        api_key: str,
+        max_temp=1.0,
+        min_temp=0.0,
+        temp=0.5,
+        model=OPENROUTER_DEFAULT_MODEL,
+        model_name=OPENROUTER_DEFAULT_MODEL_NAME,
+    ):
+        client = AsyncOpenAI(api_key=api_key, base_url="https://openrouter.ai/api/v1")
+        super().__init__(
+            api_key,
+            client,
+            model,
+            model_name,
         )
-        end_time = time.time()
-        execution_time = round(end_time - start_time, 4)
-        model_name = (
-            OPENROUTER_DEFAULT_MODEL_NAME
-            if model == OPENROUTER_DEFAULT_MODEL
-            else model
-        )
-        data = {
-            "model": model_name,
-            "output": response.choices[0].message.content,
-            "execution_time": execution_time,
-            "temp": self.temp,
-        }
-        return data
+        self.temp = temp
+        self.temp_max = max_temp
+        self.temp_min = min_temp
